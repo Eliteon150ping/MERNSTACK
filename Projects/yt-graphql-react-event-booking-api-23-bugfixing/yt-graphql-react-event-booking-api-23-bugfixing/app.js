d89a6bd383903e8dceb01770fde397492fc1751d
Rishen <rishensurjoobhalee@example.com>
2024-06-19 09:11:26 +0200

    test3

--- Projects/yt-graphql-react-event-booking-api-23-bugfixing/yt-graphql-react-event-booking-api-23-bugfixing/app.js
+++ Projects/yt-graphql-react-event-booking-api-23-bugfixing/yt-graphql-react-event-booking-api-23-bugfixing/app.js
@@ -33,7 +33,7 @@ app.use(
 );
 
 mongoose
-  .connect(`mongodb+srv:${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.q77bea6.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority&appName=Cluster0`)
+  .connect(`mongodb+srv://R1:R1234@cluster0.q77bea6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => {
     app.listen(8000);
   })
